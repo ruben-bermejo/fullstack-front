640ebfb191ce6f99fe343a4f82333405538255ed
Rubén Bermejo Sanz <petergol@gmail.com>
2023-06-11 19:29:05 +0200

    Microactividad 9

---
+++ M4/portfolio-reactjs/src/custom/customComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './customComponent.scss';
+
+export default class CustomComponent extends React.Component {
+
+    render() {
+        return (
+            <h1>Soy un H1 personalizado</h1>
+        );
+
+    }
+
+}
